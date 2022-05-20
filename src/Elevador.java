a301a5007ad8640588fdec04ba94cb213a329b38
Natália Aragão <naty-aragao@hotmail.com>
2022-05-19 23:27:07 -0300

    some functions

--- src/Elevador.java
+++ src/Elevador.java
@@ -1,21 +1,63 @@
 public class Elevador {
 
+    private Fila fila;
+    private Filas filas;
     private int[] numeroDeLugares = new int[4];
 
-    public synchronized int[] carregaEsquiador(Esquiador esquiador){
+    public synchronized void carregaEsquiador(Esquiador esquiador){
         System.out.println("Carrega esquiadores");
-        for(int i = 0; i < numeroDeLugares.length; i++){
-            numeroDeLugares[i] = esquiador.getNumeroDeEsquiadores();
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        if(filas.getEsquerdaTripla().length >= 3 || filas.getDireitaTripla().length >= 3){
+            redistributeTriple(esquiador);
+//            if(filas.getDireitaSozinho().length >= 1 || filas.getEsquerdaSozinho().length >= 1){
+//
+//            }
         }
+        chooseQueue();
+
 
         System.out.println("Lista de Esquiadores: " + numeroDeLugares);
-        return numeroDeLugares;
     }
 
+    public void redistributeTriple(Esquiador esquiador){
+        for(int i = 0; i < numeroDeLugares.length - 1; i++){
+            numeroDeLugares[i] = esquiador.getNumeroDeEsquiadores();
+//            try {
+//                Thread.sleep(200);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public void redistributeOnly(Esquiador esquiador){
+        for(int i = 0; i < numeroDeLugares.length - 3; i++){
+            numeroDeLugares[i] = esquiador.getNumeroDeEsquiadores();
+//            try {
+//                Thread.sleep(200);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public void chooseQueue(){
+        switch (this.fila) {
+            case LS:
+                this.fila = Fila.LS;
+                break;
+            case LT:
+                this.fila = Fila.LT;
+                break;
+            case RT:
+                this.fila = Fila.RT;
+                break;
+            case RS:
+                this.fila = Fila.RS;
+                break;
+            default:
+                break;
+        }
+    }
 
 }
--- src/Esquiador.java
+++ src/Esquiador.java
@@ -1,12 +1,26 @@
 public class Esquiador {
 
+    private int id;
     private int numeroDeEsquiadores = 120;
 
     public Esquiador() {
 
     }
 
+    public void entraNaFilaSozinho (){
+//        if(){
+//
+//        }
+    }
     public int getNumeroDeEsquiadores() {
         return numeroDeEsquiadores;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
