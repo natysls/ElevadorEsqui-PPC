ace4b24d997937676389747bef10fc3d83b1af9f
Patrick Andrade <eduardo.patrick.98@gmail.com>
2022-06-04 13:47:06 -0300

    Add main module with functions and base class

---
+++ src/Esquiador.java
@@ -0,0 +1,15 @@
+public class Esquiador {
+   private String nome;
+
+    public Esquiador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
