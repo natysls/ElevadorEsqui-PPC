f8859a63326587552652da29c19a8c84be71d4d2
Natália Aragão <naty-aragao@hotmail.com>
2022-06-22 01:23:34 -0300

    lista de esquiadores

--- src/Esquiador.java
+++ src/Esquiador.java
@@ -1,18 +1,12 @@
 import java.util.ArrayList;
 
-public class Esquiador{
+public class Esquiador {
     
     private String nome;
-    private ArrayList<Esquiador> listaEsquiadores = new ArrayList<>();
+    static private ArrayList<Esquiador> listaEsquiadores = new ArrayList<>();
 
     public Esquiador(String nome) {
         this.nome = nome;
-    }
-
-    private void popularListaEsquiadores(){
-        for(int i = 1; i <= 120; i++){
-            listaEsquiadores.add(new Esquiador("Esquiador " + i));
-        }
     }
 
     public static void inserirEsquiador() {
@@ -24,31 +18,39 @@ public class Esquiador{
         boolean RsTwiceLessThanRT = Filas.RS.size() < 2 * Filas.RT.size();
         boolean RsLessThanLS = Filas.RS.size() <= Filas.LS.size();
 
-        Esquiador esquiador = new Esquiador("Esquiador ");
+        int posicao = listaEsquiadores.size();
+        listaEsquiadores.add(new Esquiador("Esquiador " + (posicao + 1)));
+        Esquiador esquiador = listaEsquiadores.get(posicao);
 
         if (LsTwiceLessThanLT && LsTwiceLessThanRT && LsLessThanRS) {
             Filas.LS.add(esquiador);
             String sizes = "(" + Filas.LS.size() + "-" + Filas.LT.size() + "-" + Filas.RT.size() + "-" + Filas.RS.size() + ")";
             System.out.println("O " + esquiador.getNome() + " entrou na LS " + sizes);
-        
+
         } else if (RsTwiceLessThan && RsTwiceLessThanRT && RsLessThanLS) {
             Filas.RS.add(esquiador);
             String sizes = "(" + Filas.LS.size() + "-" + Filas.LT.size() + "-" + Filas.RT.size() + "-" + Filas.RS.size() + ")";
             System.out.println("O " + esquiador.getNome() + " entrou na RS " + sizes);
-        
+
         } else if (Filas.LT.size() <= Filas.RT.size()) {
             Filas.LT.add(esquiador);
             String sizes = "(" + Filas.LS.size() + "-" + Filas.LT.size() + "-" + Filas.RT.size() + "-" + Filas.RS.size() + ")";
             System.out.println("O " + esquiador.getNome() + " entrou na LT " + sizes);
-        
+
         } else {
             Filas.RT.add(esquiador);
             String sizes = "(" + Filas.LS.size() + "-" + Filas.LT.size() + "-" + Filas.RT.size() + "-" + Filas.RS.size() + ")";
             System.out.println("O " + esquiador.getNome() + " entrou na RT " + sizes);
-        
+
         }
+
     }
 
+    public static void acabaramEsquiadores(){
+        String sizes = "(" + Filas.LS.size() + "-" + Filas.LT.size() + "-" + Filas.RT.size() + "-" + Filas.RS.size() + ")";
+        System.out.println("Acabaram os esquiadores " + sizes);
+    }
+    
     public String getNome() {
         return nome;
     }
