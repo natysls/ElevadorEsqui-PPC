75cea68fd30e6b7535cf4f46500cd080e6df9e9c
Natália Aragão <naty-aragao@hotmail.com>
2022-06-22 01:24:11 -0300

    limite de esquiadores

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("inicio");
 
-        // try{
-        
-        //     inserirEsquiador(i);
-        //     Thread.sleep(1000);
+         Timer timerEsquiador = new Timer();
+         timerEsquiador.scheduleAtFixedRate(new TimerTask() {
+             int count = 0;
+             public void run() {
+                 try {
+                     boolean flag = true;
 
-        //     elevadorPassando();
-        //     Thread.sleep(5000);
-        
-        // }catch(InterruptedException e){
-        //     e.printStackTrace();
-        // }
+                     while(flag) {
+                         if (count == 120) {
+                             Esquiador.acabaramEsquiadores();
+                             break;
+                         }
+                         Esquiador.inserirEsquiador();
+                         count++;
+                         flag = false;
+                     }
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }
+         }, 1000, 1000);
 
-        Timer timerEsquiador = new Timer();
-        timerEsquiador.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                try {
-                    Esquiador.inserirEsquiador();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 1000, 1000);
+         Timer timerElevador = new Timer();
+         timerElevador.scheduleAtFixedRate(new TimerTask() {
+             public void run() {
+                 try {
+                     Elevador.elevadorPassando();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }
+         }, 5000, 5000);
+     }
 
-        Timer timerElevador = new Timer();
-        timerElevador.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                try {
-                    Elevador.elevadorPassando();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 5000, 5000);
-    }
-    
 }
