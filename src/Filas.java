0f28b005d6e93a4613668934ad6a873c248c7965
Natália Aragão <naty-aragao@hotmail.com>
2022-07-04 11:20:17 -0300

    movi variaveis para Elevador

--- src/Filas.java
+++ src/Filas.java
@@ -6,7 +6,4 @@ public class Filas { //classe auxiliar
     static ArrayList<Esquiador> LT = new ArrayList<>(); // Fila de uma tripla no lado esquerdo
     static ArrayList<Esquiador> RT = new ArrayList<>(); // Fila de uma tripla no lado direito
     static ArrayList<Esquiador> RS = new ArrayList<>(); // Fila de uma pessoa no lado direito
-
-    static boolean isLTturn = true;
-    static boolean isLSturn = true;
 }
